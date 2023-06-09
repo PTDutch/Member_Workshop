195a5cb0167c192f0094734545de99a5ccb3eb24
PTDutch <dutch0917@gmail.com>
2023-06-09 11:53:30 +0700

    Test

---
+++ ASP_MEMBER_WORKSHOP/Interfaces/IMemberService.cs
@@ -0,0 +1,16 @@
+﻿using ASP_MEMBER_WORKSHOP.Entitiy;
+using ASP_MEMBER_WORKSHOP.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASP_MEMBER_WORKSHOP.Interfaces
+{
+    internal interface IMemberService
+    {
+        IEnumerable<Members> MemberItems { get; }
+        void UpdateProfile(string email, ProfileModel model);
+    }
+}
---
+++ ASP_MEMBER_WORKSHOP/Services/MemberService.cs
@@ -0,0 +1,41 @@
+﻿using ASP_MEMBER_WORKSHOP.Entitiy;
+using ASP_MEMBER_WORKSHOP.Interfaces;
+using ASP_MEMBER_WORKSHOP.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASP_MEMBER_WORKSHOP.Services
+{
+    public class MemberService : IMemberService
+    {
+        private DatabaseEntities db = new DatabaseEntities();
+
+        public IEnumerable<Members> MemberItems => this.db.Members.ToList();
+
+        public void UpdateProfile(string email, ProfileModel model)
+        {
+            try
+            {
+                var memberItem = this.db.Members.SingleOrDefault(item => item.email.Equals(email));
+                if (memberItem == null) throw new Exception("Not found member");
+                this.db.Members.Attach(memberItem);
+                memberItem.firstname = model.firstname;
+                memberItem.lastname = model.lastname;
+                memberItem.position = model.position;
+                memberItem.updated = DateTime.Now;
+
+                memberItem.image = null;
+
+                this.db.Entry(memberItem).State = System.Data.Entity.EntityState.Modified;
+                this.db.SaveChanges();
+
+            }
+            catch(Exception ex)
+            {
+                throw ex.GetErrorException();
+            }
+        }
+    }
+}
